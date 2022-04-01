e436884ac913cbfed70f9dc63c76ec2b0e82cd77
Gabriel <101403161+gabriel270405@users.noreply.github.com>
2022-04-01 08:46:33 -0300

    Add files via upload

---
+++ trabalho/unidade.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+//biblioteca para usar em um arquivo final
+typedef struct unidade
+{ 
+    char unidade[100];
+    char local[100];
+    char horariofuncionamento[30];
+    char telefone[30];
+
+}unidade;
+
